d80059e3cfb93654a33fa4ffdcda45dc34e866c1
Antonio Bertuccio <anto.bertu@gmail.com>
2023-12-23 19:36:13 +0100

    Adjust default form values and enhance UI interactivity
    Updated several default input values to improve the initial user
    experience. Enhanced UI interactivity by implementing dynamic color and
    border style adjustments via JavaScript. Changes include:
    - Lowered default slider height and set initial border color to red.
    - Modified minimum value limits for certain numeric inputs.
    - Replaced regular functions with arrow functions in event listeners for
    cleaner code.
    - Added border width and style customizations in both the UI and
    corresponding handling in JavaScript.
    - Included a mechanism to adjust border color dynamically, coupled with
    the introduction of new input elements for color and styling.
    
    These improvements are part of the ongoing effort to make the UI more
    intuitive and responsive to user input, ensuring a more engaging
    experience.
    
    Ref: UIUX-2023-Enhancements

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const rangeInput = document.getElementById("range-1");
 
   // width
   const customWidth = document.getElementById("custom-width");
 
-  customWidth.addEventListener("input", function () {
+  customWidth.addEventListener("input", () => {
     const widthValue = customWidth.value;
     console.log("Width Value: ", widthValue);
 
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // height
   const customHeight = document.getElementById("custom-height");
 
-  customHeight.addEventListener("input", function () {
+  customHeight.addEventListener("input", () => {
     const heightValue = customHeight.value;
     console.log("Height Value: ", heightValue);
 
@@ -22,19 +22,66 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // enable background-color
-
   const enableBgColorCheckbox = document.getElementById("enable-bg-color");
   const customBgColor = document.getElementById("custom-bg-color");
 
-  enableBgColorCheckbox.addEventListener("change", function () {
+  enableBgColorCheckbox.addEventListener("change", () => {
     customBgColor.toggleAttribute("disabled");
     rangeInput.classList.toggle("enabled-bg-color");
   });
 
   // background-color
-  customBgColor.addEventListener("input", function () {
+  customBgColor.addEventListener("input", () => {
     const bgColorValue = customBgColor.value;
-
     rangeInput.style.backgroundColor = bgColorValue;
+  });
+
+  // border-width
+  const borderWidthInput = document.getElementById("border-width");
+
+  borderWidthInput.addEventListener("input", () => {
+    const borderWidthValue = borderWidthInput.value;
+    rangeInput.style.border = borderWidthValue + "px solid #ff0000";
+  });
+
+  // border-style
+  const borderStyleSelect = document.getElementById("border-style");
+
+  borderStyleSelect.addEventListener("change", () => {
+    const borderStyleValue = borderStyleSelect.value;
+    rangeInput.style.borderStyle = getBorderStyle(borderStyleValue);
+  });
+
+  function getBorderStyle(value) {
+    switch (value) {
+      case "1":
+        return "dotted";
+      case "2":
+        return "dashed";
+      case "3":
+        return "double";
+      case "4":
+        return "groove";
+      case "5":
+        return "ridge";
+      case "6":
+        return "inset";
+      case "7":
+        return "outset";
+      case "8":
+        return "none";
+      case "9":
+        return "hidden";
+      default:
+        return "solid";
+    }
+  }
+
+  // custom-border-color
+  const customBorderColorInput = document.getElementById("custom-border-color");
+
+  customBorderColorInput.addEventListener("input", () => {
+    const borderColorValue = customBorderColorInput.value;
+    rangeInput.style.borderColor = borderColorValue;
   });
 });
